7dc67b9a4aea7439e9182c51545ac3b4b93a59de
Abhay Kumar <kumarabhay26111996@gmail.com>
2025-02-05 11:50:51 +0530

    Initial commit - Merging Vite project with repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-500 text-3xl font-bold underline">
+        Hello, Welcome to MASAI SCHOOL
+      </h1>
+    </div>
+  );
+}
+
+export default App;
